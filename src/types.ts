40d2c86866a0ad687926f80db3d4e52beb1ea0b2
Gajus Kuizinas <gajus@gajus.com>
2021-11-06 22:40:14 -0600

    feat: remove unused fields

--- src/types.ts
+++ src/types.ts
@@ -22,14 +22,12 @@ export type RelationalOperator = '<' | '<=' | '=' | '>' | '>=';
 
 export type Ast = {
   field: string,
-  fieldLocation?: Location,
   left?: Ast,
   operand?: Ast,
   operator?: '&&' | '||' | 'AND' | 'NOT' | 'OR',
   quoted?: boolean,
   range?: Range,
   regex?: boolean,
-  relationalOperator?: RelationalOperator,
   right: Ast,
   term: string,
   termLocation?: Location,
