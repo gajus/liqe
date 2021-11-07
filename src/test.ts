06def47db23001c91a5708fb9ba32e523a6d99f1
Gajus Kuizinas <gajus@gajus.com>
2021-11-06 19:09:07 -0600

    fix: correct types import path

--- src/test.ts
+++ src/test.ts
@@ -1,9 +1,9 @@
-import type {
-  Ast,
-} from '../types';
 import {
   filter,
 } from './filter';
+import type {
+  Ast,
+} from './types';
 
 export const test = <T extends Object>(ast: Ast, subject: T) => {
   return filter(ast, [subject]).length === 1;
