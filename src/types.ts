72cc6e9459a7211278dded1e3117f277f05576d9
Gajus Kuizinas <gajus@gajus.com>
2021-11-06 22:41:23 -0600

    feat: remove unused fields

--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,3 @@
-type Location = {
-  end: {
-    column: number,
-    line: number,
-    offset: number,
-  },
-  start: {
-    column: number,
-    line: number,
-    offset: number,
-  },
-};
-
 export type Range = {
   max: number,
   maxInclusive: boolean,
@@ -24,12 +11,12 @@ export type Ast = {
   field: string,
   left?: Ast,
   operand?: Ast,
-  operator?: 'AND' | 'NOT' | 'OR',
+  operator?: '&&' | '||' | 'AND' | 'NOT' | 'OR',
   quoted?: boolean,
   range?: Range,
   regex?: boolean,
+  relationalOperator?: RelationalOperator,
   right: Ast,
   term: string,
-  termLocation?: Location,
   test?: (subject: string) => boolean,
 };
