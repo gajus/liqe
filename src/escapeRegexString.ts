3ee803a3a63ff67fbb92da4b4db3188e01cab90e
Gajus Kuizinas <gajus@gajus.com>
2021-11-10 05:07:58 -0600

    perf: avoid recreating regex

--- src/escapeRegexString.ts
+++ src/escapeRegexString.ts
@@ -1,5 +1,8 @@
+const ESCAPE_RULE = /[$()*+.?[\\\]^{|}]/g;
+const DASH_RULE = /-/g;
+
 export const escapeRegexString = (subject: string): string => {
   return subject
-    .replace(/[$()*+.?[\\\]^{|}]/g, '\\$&')
-    .replace(/-/g, '\\x2d');
+    .replace(ESCAPE_RULE, '\\$&')
+    .replace(DASH_RULE, '\\x2d');
 };
